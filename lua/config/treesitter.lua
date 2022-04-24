a46f453ff47296535eb0ac371723d300bde3d13e
Eric Villard <dev@eviweb.fr>
2022-04-24 14:27:03 +0200

    feat(plugin): ✨ add nvim-treesitter-textobjects
    Signed-off-by: Eric Villard <dev@eviweb.fr>

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -3,11 +3,11 @@
 local treesitter = require('nvim-treesitter.configs')
 
 treesitter.setup({
-    ensure_installed = "all",
+    ensure_installed = 'all',
     sync_installed = true,
     highlight = {
         enable = true,
-        additional_vim_regex_highlighting = { "php" },
+        additional_vim_regex_highlighting = { 'php' },
     },
     indent = {
         enable = false,
@@ -15,10 +15,10 @@ treesitter.setup({
     incremental_selection = {
         enable = true,
         keymaps = {
-            init_selection = "gnn",
-            node_incremental = "grn",
-            scope_incremental = "grc",
-            node_decremental = "grm",
+            init_selection = 'gnn',
+            node_incremental = 'grn',
+            scope_incremental = 'grc',
+            node_decremental = 'grm',
         },
     },
     rainbow = {
@@ -26,6 +26,56 @@ treesitter.setup({
         disable = {},
         extended_mode = true,
         max_file_lines = nil,
+    },
+    textobjects = {
+        select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+                ['aF'] = '@custom-capture',
+                ['af'] = '@function.outer',
+                ['if'] = '@function.inner',
+                ['ac'] = '@class.outer',
+                ['ic'] = '@class.inner',
+            },
+        },
+        swap = {
+            enable = true,
+            swap_next = {
+                ['<leader>a'] = '@parameter.outer',
+            },
+            swap_previous = {
+                ['<leader>A'] = '@parameter.inner',
+            },
+        },
+        move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+                [']m'] = '@function.outer',
+                [']]'] = '@class.outer',
+            },
+            goto_next_end = {
+                [']M'] = '@function.outer',
+                [']['] = '@class.outer',
+            },
+            goto_previous_start = {
+                ['[m'] = '@function.outer',
+                ['[['] = '@class.outer',
+            },
+            goto_previous_end = {
+                ['[M'] = '@function.outer',
+                ['[]'] = '@class.outer',
+            },
+        },
+        lsp_interop = {
+            enable = true,
+            border = 'none',
+            peek_definition_code = {
+                ['<leader>df'] = '@function.outer',
+                ['<leader>dF'] = '@class.outer',
+            },
+        },
     },
 })
 
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,6 +5,9 @@ local use = require('packer').use
 use({
     'nvim-treesitter/nvim-treesitter',
 	run = ':TSUpdate',
+    requires = {
+        'nvim-treesitter/nvim-treesitter-textobjects'
+    },
     config = function()
         require("config.treesitter")
     end,
