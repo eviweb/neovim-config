5f3c945bcb50a28522f25bcb19801cd759834e49
Eric Villard <dev@eviweb.fr>
2022-04-25 17:33:14 +0200

    chore(plugin): 🧹 add nvim-cmp source for Lua API
    Signed-off-by: Eric Villard <dev@eviweb.fr>

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -7,11 +7,12 @@ use({
     requires = {
         'onsails/lspkind-nvim',
         'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-nvim-lua',
         'hrsh7th/cmp-buffer',
         'hrsh7th/cmp-path',
         'hrsh7th/cmp-cmdline',
         'saadparwaiz1/cmp_luasnip',
-        'L3MON4D3/LuaSnip',        
+        'L3MON4D3/LuaSnip',
         'rafamadriz/friendly-snippets',
     },
     config = function()
